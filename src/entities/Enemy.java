e492e4cf8a5b38e5ddfae02a5d6cecee1117b23f
Joseph Huang <69940801+jhuang915@users.noreply.github.com>
2021-05-06 08:41:42 -0700

    added method headers to enemy class

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -1,5 +1,26 @@
 package entities;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.*;
+import processing.core.PImage;
+import Maze.*;
 
-public class Enemy {
 
+public class Enemy extends MovingImage {
+
+	private double health;
+	private int x,y;
+	
+	public Enemy(PImage img, int x, int y, int w, int h) {
+		super(img, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void move() {
+		
+	}
+	
+	public void attack() {
+		
+	}
 }
