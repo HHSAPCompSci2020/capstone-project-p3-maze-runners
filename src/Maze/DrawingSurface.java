8b3b148a293e8b2b4c328a7f258930ee94c3dacc
Joseph Huang <69940801+jhuang915@users.noreply.github.com>
2021-05-06 13:49:22 -0700

    updated abilty and drawingsurface class

--- src/Maze/DrawingSurface.java
+++ src/Maze/DrawingSurface.java
@@ -22,41 +22,45 @@ public class DrawingSurface extends PApplet {
 	private ArrayList<Shape> obstacles;
 
 	private ArrayList<Integer> keys;
-	
+
 	//Our fields
 	private ArrayList<Maze> allMazes;
 	private Maze maze0, maze1;//to be implemented
 	private static long iterations = 0;
-	
+
 	/**mazeSelected is the index of allMazes that will be loaded on screen
 	 * 
 	 */
 	public int mazeSelected = 0;
-	
+
 	/**
 	 * mazeChangeCooldown is the number of draw() method calls that pass between each maze change.
 	 * So at mazeChangeCooldown = 30, you can change the maze at most once per 30 frames or once per 0.5 seconds
 	 */
 	public int mazeChangeCooldown;
-	
-	
+
+	/**
+	 * how many abilities are to be spawned randomly in the maze
+	 */
+	public int abilityNum;
+
 
 	public DrawingSurface() {
 		super();
 		keys = new ArrayList<Integer>();
 		screenRect = new Rectangle(0,0,DRAWING_WIDTH,DRAWING_HEIGHT);
 		obstacles = new ArrayList<Shape>();
-//		obstacles.add(new Rectangle(200,400,400,50));
-//		obstacles.add(new Rectangle(0,250,100,50));
-//		obstacles.add(new Rectangle(700,250,100,50));
-//		obstacles.add(new Rectangle(375,300,50,100));
-//		obstacles.add(new Rectangle(300,250,200,50));
-		
-		
+		//		obstacles.add(new Rectangle(200,400,400,50));
+		//		obstacles.add(new Rectangle(0,250,100,50));
+		//		obstacles.add(new Rectangle(700,250,100,50));
+		//		obstacles.add(new Rectangle(375,300,50,100));
+		//		obstacles.add(new Rectangle(300,250,200,50));
+
+
 		String instructions = "Use arrow keys or WASD to move. \n"
 				+ "Press 'm' to toggle which Maze is on screen, which only works every half second";
 		System.out.println(instructions);
-		
+
 		maze1 = new Maze();
 		maze0 = new Maze();
 		allMazes = new ArrayList<Maze>();
@@ -70,22 +74,32 @@ public class DrawingSurface extends PApplet {
 		maze1.add(new Rectangle(300,300,100,w));
 		maze1.add(new Rectangle(400,300,w,100));
 		maze1.add(new Rectangle(400,400,200,w));
-		
+
 		allMazes.add(maze1);
 		maze0.add(new Rectangle(200,400,400,50));
 		maze0.add(new Rectangle(0,250,100,50));
 		maze0.add(new Rectangle(700,250,100,50));
 		maze0.add(new Rectangle(375,300,50,100));
 		maze0.add(new Rectangle(300,250,200,50));
-		
+
 		allMazes.add(maze0);
-		
+
 		spawnNewMario();
 	}
 
 
 	public void spawnNewMario() {
 		mario = new Player(loadImage("notmario.png"), DRAWING_WIDTH/2-Player.MARIO_WIDTH/2,50);
+	}
+
+	public void spawnNewEnemy() {
+
+	}
+
+	public void spawnNewAbility() {
+ for(int i=0; i< abilityNum; i++) {
+	 
+ }
 	}
 
 	// The statements in the setup() function 
@@ -100,22 +114,22 @@ public class DrawingSurface extends PApplet {
 	// line is executed again.
 	public void draw() {
 		if (iterations%10 == 0)
-//			System.out.println("iteration: "+iterations);
-		iterations++;
+			//			System.out.println("iteration: "+iterations);
+			iterations++;
 		if (mazeChangeCooldown > 0) {
 			mazeChangeCooldown--;
 		}
 		//Loading walls 
-		
+
 		obstacles = new ArrayList<Shape>();
 		for (Shape s: allMazes.get(mazeSelected).getWalls()) {
 			obstacles.add(s);
 		}
-		
-		
-		
+
+
+
 		// drawing stuff
-		
+
 		background(128,212,255);   
 
 		pushMatrix();
@@ -141,72 +155,72 @@ public class DrawingSurface extends PApplet {
 
 		popMatrix();
 
-		
+
 		// modifying stuff
 
 		if (isPressed(KeyEvent.VK_LEFT))
 			mario.moveBy(-1,0);
-//			mario.walk(-1);
+		//			mario.walk(-1);
 		if (isPressed(KeyEvent.VK_RIGHT))
 			mario.moveBy(1,0);
-//			mario.walk(1);
+		//			mario.walk(1);
 		if (isPressed(KeyEvent.VK_UP))
 			mario.moveBy(0,-1);
-//			mario.jump();
+		//			mario.jump();
 		if (isPressed(KeyEvent.VK_DOWN))
 			mario.moveBy(0, 1);
-		
+
 		//Chris: you can also use WASD to move
 		if (isPressed(KeyEvent.VK_A))
 			mario.moveBy(-1,0);
-//			mario.walk(-1);
+		//			mario.walk(-1);
 		if (isPressed(KeyEvent.VK_D))
 			mario.moveBy(1,0);
-//			mario.walk(1);
+		//			mario.walk(1);
 		if (isPressed(KeyEvent.VK_W))
 			mario.moveBy(0,-1);
-//			mario.jump();
+		//			mario.jump();
 		if (isPressed(KeyEvent.VK_S))
 			mario.moveBy(0, 1);
-		
+
 		if (isPressed(KeyEvent.VK_M)) { //toggle which maze
 			if (mazeChangeCooldown == 0) {
 				mazeChangeCooldown = 30;
 				mazeSelected++;
 			}
-				
-			
-			
-			
+
+
+
+
 			if (mazeSelected >= allMazes.size()) {
 				mazeSelected = 0;
 			}
 		}
-		
+
 		mario.act(obstacles);
 
 		if (!screenRect.intersects(mario))
 			spawnNewMario();
 
-//		//draw gridLines but they dont scale with window
-//
-//		for (int i = 1; i <= width/100; i++) {
-//			pushStyle();
-//			this.color(0,0,0, 16);
-//			//horizontal lines
-//			line( 0, 100*i, DRAWING_WIDTH, 100*i);
-//			//vertical lines
-//			popStyle();
-//		}
-//		for (int i = 1; i <= DRAWING_WIDTH/100; i++) {
-//			pushStyle();
-//			this.color(0,0,0, 200);
-//			//horizontal lines
-//			line( 100*i, 0, 100*i , DRAWING_HEIGHT);
-//			//vertical lines
-//			popStyle();
-//
-//		}
+		//		//draw gridLines but they dont scale with window
+		//
+		//		for (int i = 1; i <= width/100; i++) {
+		//			pushStyle();
+		//			this.color(0,0,0, 16);
+		//			//horizontal lines
+		//			line( 0, 100*i, DRAWING_WIDTH, 100*i);
+		//			//vertical lines
+		//			popStyle();
+		//		}
+		//		for (int i = 1; i <= DRAWING_WIDTH/100; i++) {
+		//			pushStyle();
+		//			this.color(0,0,0, 200);
+		//			//horizontal lines
+		//			line( 100*i, 0, 100*i , DRAWING_HEIGHT);
+		//			//vertical lines
+		//			popStyle();
+		//
+		//		}
 	}
 
 
