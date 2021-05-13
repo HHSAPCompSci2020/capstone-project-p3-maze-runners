bc7173aaae28b1fba345256f80f918fdef41f04a
Joseph Huang <69940801+jhuang915@users.noreply.github.com>
2021-05-13 13:47:22 -0700

    heal code update

--- src/entities/Heal.java
+++ src/entities/Heal.java
@@ -5,11 +5,16 @@ public class Heal extends Ability{
 	Creature a ;
 	public Heal(PImage img, int x, int y, int width, int height) {
 		super(img, x, y, width, height);
+		super.uses=1;
+
 	}
 	public void healing(int x) {
 		//super.health+=x; this method if we make ability extend creature not moving image
 	}
 	public void healing2() {
 		a.heal(1);
+	}
+	public void use() {
+		super.use();
 	}
 }
