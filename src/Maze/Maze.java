62e3e4c56e390d0cf5978949d5491e556829f9af
LakshyaShrivastava <lshrivastava169@student.fuhsd.org>
2021-05-18 19:28:13 -0700

    Fixed formatting for Main.java & Maze.java

--- src/Maze/Maze.java
+++ src/Maze/Maze.java
@@ -16,25 +16,25 @@ import entities.enemies.TimingTrap;
 import entities.*;
 
 public class Maze {
-	private ArrayList<Enemy> enemies;
-	private ArrayList<Ability> abilities;
-	private ArrayList<Shape> walls;
-	private ArrayList<Exit> exits;
-	
+
+	// characters that represents objects in grid
+	private final char START_LOCATION = 'C', WALL = '#', WALKABLE = '.', HEAL_CROSS = 'h', STEALTH = 's',
+			TIMING_TRAP = 'T', EXIT = 'X', SPIKE = 'S';
+
 	public int playerStartX, playerStartY;
 	/**
-	 * grid is a 2D char array initialized from Maze's constructor, indexes are at [row #][col #]
+	 * grid is a 2D char array initialized from Maze's constructor, indexes are at
+	 * [row #][col #]
 	 */
 	protected char[][] grid;
 	private final int DEFAULT_MAZE_WIDTH = 20, DEFAULT_MAZE_HEIGHT = 20;
 	private int cellWidth = 40, cellHeight = 40;
-	
-	//characters that represents objects in grid
-	private final char START = 'C', WALL = '#', WALKABLE = '.';
-	
-	
-	
-	
+
+	private ArrayList<Enemy> enemies;
+	private ArrayList<Ability> abilities;
+	private ArrayList<Shape> walls;
+	private ArrayList<Exit> exits;
+
 	public Maze() {
 		walls = new ArrayList<Shape>();
 		enemies = new ArrayList<Enemy>();
@@ -42,43 +42,41 @@ public class Maze {
 		exits = new ArrayList<Exit>();
 
 		grid = new char[DEFAULT_MAZE_HEIGHT][DEFAULT_MAZE_WIDTH];
-		playerStartX = 25; 
+		playerStartX = 25;
 		playerStartY = 25;
 	}
-	
+
 	public Maze(PApplet marker, String filename, int gridWidth, int gridHeight) {
 		walls = new ArrayList<Shape>();
 		enemies = new ArrayList<Enemy>();
 		abilities = new ArrayList<Ability>();
 		exits = new ArrayList<Exit>();
-		
-		
+
 		grid = new char[gridHeight][gridWidth];
 		this.readData(filename, grid);
 		addObjectsFromGrid(marker);
 	}
-	
-	
+
 	/**
-	 * @return the reference to the ArrayList of Shapes representing the walls in a particular maze
+	 * @return the reference to the ArrayList of Shapes representing the walls in a
+	 *         particular maze
 	 */
-	public ArrayList<Shape> getWalls(){
+	public ArrayList<Shape> getWalls() {
 		return walls;
 	}
-	
-	public ArrayList<Enemy> getEnemies(){
+
+	public ArrayList<Enemy> getEnemies() {
 		return enemies;
 	}
-	
-	public ArrayList<Ability> getAbilities(){
+
+	public ArrayList<Ability> getAbilities() {
 		return abilities;
 	}
-	
-	public ArrayList<Exit> getExits()
-	{
+
+	public ArrayList<Exit> getExits() {
 		return exits;
 	}
-	
+
 	/**
 	 * 
 	 * @param shape the Shape object to add to the maze, should be a Rectangle
@@ -86,108 +84,82 @@ public class Maze {
 	public void addWall(Shape shape) {
 		walls.add(shape);
 	}
-	
+
 	/**
-	 * adds a Creature to the maze. allows Enemies  too because of polymorphism
+	 * adds a Creature to the maze. allows Enemies too because of polymorphism
 	 */
 	public void addCreature(Creature creature) {
-		enemies.add((Enemy)creature);
+		enemies.add((Enemy) creature);
 	}
+
 	public void addEnemy(Enemy e) {
 		enemies.add(e);
 	}
+
 	public void addAbility(Ability ability) {
 		abilities.add(ability);
 	}
-	
-	public void addExit(Exit e)
-	{
+
+	public void addExit(Exit e) {
 		exits.add(e);
-//		System.out.println(exits.toString());
 	}
-	
+
 	private void addObjectsFromGrid(PApplet marker) {
-		for (int row = 0; row < grid.length; row ++) {
+		for (int row = 0; row < grid.length; row++) {
 			for (int col = 0; col < grid[row].length; col++) {
 				char c = grid[row][col];
-				int x = col*cellWidth;
-				int y = row*cellHeight;
-				
-				
-				
-				if (c == 'h') {//heal cross
-					this.addAbility(new Heal(null, x, y, cellWidth, cellHeight ));
+				int x = col * cellWidth;
+				int y = row * cellHeight;
+
+				if (c == HEAL_CROSS) {
+					this.addAbility(new Heal(null, x, y, cellWidth, cellHeight));
 				}
-				
-				if (c == 's') {//stealth
-					this.addAbility(new Stealth(null, x, y, cellWidth, cellHeight ));
+
+				if (c == STEALTH) {
+					this.addAbility(new Stealth(null, x, y, cellWidth, cellHeight));
 				}
-				
-				if (c == 'T') {//TimingTrap
-					TimingTrap temp = new TimingTrap(marker.loadImage("data//spike.png"), x,y, cellWidth, cellHeight);
+
+				if (c == TIMING_TRAP) {
+					TimingTrap temp = new TimingTrap(marker.loadImage("data//spike.png"), x, y, cellWidth, cellHeight);
 					this.addEnemy(temp);
-					
+
 				}
-				if(c == 'X') //Exit
-				{
+				if (c == EXIT) {
 					Exit e = new Exit(marker.loadImage("data//Exit.png"), x, y, cellWidth, cellHeight);
 					this.addExit(e);
-					
 				}
-				
-				if(c == 'S') //Exit
-				{
+
+				if (c == SPIKE) {
 					Spike e = new Spike(marker.loadImage("data//spike2.png"), x, y, cellWidth, cellHeight);
 					this.addEnemy(e);
-					
 				}
-				
-//				if (System.nanoTime() % 100 == 0) {
-//					System.out.println("timing trap symbol: " + TimingTrap.getSymbol());
-//					System.out.println("heal symbol: " + Heal.getSymbol());
-////					System.out.println()
-//
-//				}
-				
-				
-				
-				
-				if (c == WALL) {//wall
-					//Rectangle(int x, int y, int width, int height)
-					Rectangle r = new Rectangle(col*cellWidth, row * cellHeight, cellWidth, cellHeight); 
+
+				if (c == WALL) {
+					// Rectangle(int x, int y, int width, int height)
+					Rectangle r = new Rectangle(col * cellWidth, row * cellHeight, cellWidth, cellHeight);
 					walls.add(r);
 				}
-				if(c == START) {//start location
-					playerStartX = col*cellWidth;
-					playerStartY = row*cellHeight;
+				if (c == START_LOCATION) {
+					playerStartX = col * cellWidth;
+					playerStartY = row * cellHeight;
 				}
-				if(c == WALKABLE) {
-					//add nothing
+				if (c == WALKABLE) {
+					// add nothing
 				}
-				
-				
-				
-				
-//				walls.add(e)
 			}
 		}
-		
-		
-		
+
 	}
-	
-	
-	
-	
-	
+
 	/**
 	 * Copied from Recursion in 2D arrays lab: GridTemplate.java
+	 * 
 	 * @pre char[][] gameData is initialized
 	 * @post gameData will be filled with all the characters in the text file
 	 * @param filename name of the file to read characters from
 	 * @param gameData the 2D char array to store the characters in
 	 */
-	public void readData (String filename, char[][] gameData) {
+	public void readData(String filename, char[][] gameData) {
 		File dataFile = new File(filename);
 
 		if (dataFile.exists()) {
@@ -196,20 +168,20 @@ public class Maze {
 			FileReader reader = null;
 			Scanner in = null;
 			try {
-					reader = new FileReader(dataFile);
-					in = new Scanner(reader);
-					
-					while (in.hasNext()) {
-						String line = in.nextLine();
-						for(int i = 0; i < line.length(); i++)
-							if (count < gameData.length && i < gameData[count].length) {
-								if (line.charAt(i) == '\\')
-									return;
-								gameData[count][i] = line.charAt(i);
-							}
+				reader = new FileReader(dataFile);
+				in = new Scanner(reader);
 
-						count++;
-					}
+				while (in.hasNext()) {
+					String line = in.nextLine();
+					for (int i = 0; i < line.length(); i++)
+						if (count < gameData.length && i < gameData[count].length) {
+							if (line.charAt(i) == '\\')
+								return;
+							gameData[count][i] = line.charAt(i);
+						}
+
+					count++;
+				}
 
 			} catch (IOException ex) {
 				throw new IllegalArgumentException("Data file " + filename + " cannot be read.");
@@ -217,12 +189,12 @@ public class Maze {
 				if (in != null)
 					in.close();
 			}
-			
+
 		} else {
 			throw new IllegalArgumentException("Data file " + filename + " does not exist.");
 		}
 	}
-	
+
 	/**
 	 * @pre chars is a rectangular 2D char array that is initialized
 	 * @param chars
@@ -230,40 +202,31 @@ public class Maze {
 	public void printCharArray(char[][] chars) {
 		String s0 = "";
 		for (int i = 0; i < chars.length; i++) {
-			if (i!= 0)
-				s0+= "\n";
+			if (i != 0)
+				s0 += "\n";
 			for (int j = 0; j < chars[0].length; j++) {
-				
-				s0+= chars[i][j] + " ";
-				
+
+				s0 += chars[i][j] + " ";
+
 			}
 		}
 		System.out.println(s0);
 	}
-	
-//	public boolean removeCreature(Creature c) {
-//		boolean successfullyRemoved = false;
-//		if (creatures.remove(c) ) {
-//			successfullyRemoved = true;
-//		}
-//		return successfullyRemoved;
-//	}
-	
-		
-	public char[][] getGrid(){
+
+	public char[][] getGrid() {
 		return grid;
 	}
-	
+
 	public String toString() {
 		String s0 = "";
-		
+
 		for (int i = 0; i < grid.length; i++) {
-			if (i!= 0)
-				s0+= "\n";
+			if (i != 0)
+				s0 += "\n";
 			for (int j = 0; j < grid[0].length; j++) {
-				
-				s0+= grid[i][j] + " ";
-				
+
+				s0 += grid[i][j] + " ";
+
 			}
 		}
 		return s0;
