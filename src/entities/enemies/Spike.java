cbe7378b5761cf735d1bd5173f9dd587a5800008
LakshyaShrivastava <lshrivastava169@student.fuhsd.org>
2021-05-19 23:51:27 -0700

    Deleted extraneous code Formatted and added javadoc comments in Spike.java and Timingtrap.java

--- src/entities/enemies/Spike.java
+++ src/entities/enemies/Spike.java
@@ -1,90 +1,104 @@
 package entities.enemies;
 
-import Maze.DrawingSurface;
 import entities.Player;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 /**
- * @author Lakshya Shrivastava
- * A timing trap will be spikes that appear and disappear every few seconds. 
- * If the player touches the timing trap, their health will go down by 1 
+ * @author Lakshya Shrivastava A timing trap will be spikes that appear and
+ *         disappear every few seconds. If the player touches the timing trap,
+ *         their health will go down by 1
  */
+@SuppressWarnings("serial")
 public class Spike extends Enemy {
 	private boolean isVisible;
-	
-	private static char tSymbol = 'T';
-	
-	public Spike(PImage img, int x, int y)
-	{
+
+	/**
+	 * Constructs this Spike at position x,y with the given image and default
+	 * dimensions
+	 * 
+	 * @param img the PImage that is drawn for this Creature
+	 * @param x   the top left x coordinate
+	 * @param y   the top left x coordinate
+	 */
+
+	public Spike(PImage img, int x, int y) {
 		super(img, x, y);
 		isVisible = true;
-//		symbol = 'T';
-//		imageName = "spike.png";
 		attackDamage = 1;
 		canDie = false;
 	}
-	
+
+	/**
+	 * Constructs this Spike at position x,y with the given image and user provided
+	 * dimensions
+	 * 
+	 * @param img    the PImage that is drawn for this Creature
+	 * @param x      the top left x coordinate
+	 * @param y      the top left x coordinate
+	 * @param width  the width of this Creature
+	 * @param height the height of this Creature
+	 */
 	public Spike(PImage img, int x, int y, int width, int height) {
 		super(img, x, y, width, height);
 		isVisible = true;
-//		symbol = 'T';
-//		imageName = "spike.png";
 
 	}
-	
 
 	/**
+	 * Method is used for attacking a player
 	 * 
-	 * @override
+	 * @param p the player to attack
 	 */
-	public void attack(Player p) //Chris:I think this is unused?
-	{
+	public void attack(Player p) {
 		if (isVisible) {
 			p.reduceHealthBy(1, x, y, width, height);
 		}
-		else {
-//			System.out.println("safe to pass");
-		}
 	}
-	
-	/**
-	 * This method will be used to toggle whether the spike will be draw or not and if they will hurt the player or not. 
-	 * @param state the state to which the visibility will be changed
-	 */
-	private void setVisibility(boolean state)
-	{
-		isVisible = true;
-	}
-	
+
 	/**
 	 * This method does nothing because the spikes cannot be destroyed.
 	 */
-	public void reduceHealth()
-	{
+	public void reduceHealth() {
 	}
-	
+
+	/**
+	 * Returns a string representation of the Spike
+	 * 
+	 * @return the string representation of the Spike
+	 */
 	public String toString() {
-		String s = "Spike at x="+x + "y="+y;
+		String s = "Spike at x=" + x + "y=" + y;
 		return s;
 	}
 
-//	public static char getSymbol() {
-//		return tSymbol;
-//	}
-
+	/**
+	 * the method that draws the TimingTrap to the provided PApplet
+	 * 
+	 * @param marker the PApplet to draw on
+	 */
 	public void draw(PApplet marker) {
-
-
 		super.draw(marker);
 
 	}
+
+	/**
+	 * Returns whether this enemy is moveable or not. Must by implemented by all
+	 * subclasses
+	 * 
+	 * @return true if this enemy is moveable, false otherwise
+	 */
 	public boolean isMovable() {
 		return false;
 	}
-	
+
+	/**
+	 * Returns whether this enemy can die
+	 * 
+	 * @return true if enemy can die, false otherwise
+	 */
 	public boolean canDie() {
 		return false;
 	}
-	
+
 }
--- src/entities/enemies/TimingTrap.java
+++ src/entities/enemies/TimingTrap.java
@@ -6,90 +6,124 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 /**
- * A timing trap will be spikes that appear and disappear every few seconds. 
- * If the player touches the timing trap, their health will go down 
+ * A timing trap will be spikes that appear and disappear every few seconds. If
+ * the player touches the timing trap, their health will go down
+ * 
  * @author Lakshya Shrivastava
  */
 @SuppressWarnings("serial")
 public class TimingTrap extends Enemy {
 	private boolean isVisible;
-	
-	
-	public TimingTrap(PImage img, int x, int y)
-	{
+
+	/**
+	 * Constructs this TimingTrap at position x,y with the given image and default
+	 * dimensions
+	 * 
+	 * @param img the PImage that is drawn for this Creature
+	 * @param x   the top left x coordinate
+	 * @param y   the top left x coordinate
+	 */
+	public TimingTrap(PImage img, int x, int y) {
 		super(img, x, y);
 		isVisible = true;
 		attackDamage = 1;
 		canDie = false;
 	}
-	
+
+	/**
+	 * Constructs this TimingTrap at position x,y with the given image and user
+	 * provided dimensions
+	 * 
+	 * @param img    the PImage that is drawn for this Creature
+	 * @param x      the top left x coordinate
+	 * @param y      the top left x coordinate
+	 * @param width  the width of this Creature
+	 * @param height the height of this Creature
+	 */
 	public TimingTrap(PImage img, int x, int y, int width, int height) {
 		super(img, x, y, width, height);
 		isVisible = true;
 
 	}
-	
 
-	/**Attacks player p and knocks them back
+	/**
+	 * Attacks player p and knocks them back
+	 * 
 	 * @author Christopher Lew
 	 * @param p the Player being attacked
 	 * @override
 	 */
-	public void attack(Player p) //Chris:I think this is unused?
+	public void attack(Player p) // Chris:I think this is unused?
 	{
 		if (isVisible) {
 			p.reduceHealthBy(1, x, y, width, height);
-		}
-		else {
+		} else {
 //			System.out.println("safe to pass");
 		}
 	}
-	
+
 	/**
-	 * This method will be used to toggle whether the spike will be draw or not and if they will hurt the player or not. 
+	 * This method will be used to toggle whether the spike will be draw or not and
+	 * if they will hurt the player or not.
+	 * 
 	 * @param state the state to which the visibility will be changed
 	 */
-	private void setVisibility(boolean state)
-	{
+	private void setVisibility(boolean state) {
 		isVisible = state;
 	}
-	
+
 	/**
 	 * This method does nothing because the spikes cannot be destroyed.
 	 */
-	public void reduceHealth()
-	{
+	public void reduceHealth() {
 	}
-	
+
+	/**
+	 * Returns a string representation of the TimingTrap
+	 * 
+	 * @return the string representation of the TimingTrap
+	 */
 	public String toString() {
-		String s = "TimingTrap at x="+x + "y="+y;
+		String s = "TimingTrap at x=" + x + "y=" + y;
 		return s;
 	}
 
-//	public static char getSymbol() {
-//		return tSymbol;
-//	}
-
+	/**
+	 * the method that draws the TimingTrap to the provided PApplet
+	 * 
+	 * @param marker the PApplet to draw on
+	 */
 	public void draw(PApplet marker) {
-		
+
 		if (DrawingSurface.getIterations() % 120 <= 60) {
 			setVisibility(false);
 			marker.pushStyle();
 			marker.noStroke();
-			marker.fill(180,180,180, 180 );
-			marker.rect((float)x, (float)y, (float)width, (float)height);
+			marker.fill(180, 180, 180, 180);
+			marker.rect((float) x, (float) y, (float) width, (float) height);
 			marker.popStyle();
-			
-		}
-		else {
+
+		} else {
 			setVisibility(true);
 			super.draw(marker);
 		}
 	}
-	
+
+	/**
+	 * Returns whether this enemy is moveable or not. Must by implemented by all
+	 * subclasses
+	 * 
+	 * @return true if this enemy is moveable, false otherwise
+	 */
 	public boolean isMovable() {
 		return false;
 	}
+
+	/**
+	 * Returns whether this enemy can die
+	 * 
+	 * @return true if enemy can die, false otherwise
+	 */
 	public boolean canDie() {
 		return false;
 	}
