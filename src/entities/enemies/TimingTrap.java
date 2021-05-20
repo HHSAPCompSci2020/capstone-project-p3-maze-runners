9f5138b53bda5146e14f0d884991e0a55d5fce9d
Joseph Huang <69940801+jhuang915@users.noreply.github.com>
2021-05-20 13:53:55 -0700

    add maze and fixing stealth

--- src/entities/enemies/TimingTrap.java
+++ src/entities/enemies/TimingTrap.java
@@ -98,8 +98,8 @@ public class TimingTrap extends Spike {
 	 */
 	public void draw(PApplet marker) {
 
-		
-		if (DrawingSurface.getIterations() % 120 <= 60&&!pl.visibleByEnemies) {
+		//&&!pl.visibleByEnemies
+		if (DrawingSurface.getIterations() % 120 <= 60) {
 			setVisibility(false);
 			marker.pushStyle();
 			marker.noStroke();
